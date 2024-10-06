90fae258ae8954a91359abe04d212851d7484e8f
abbas ejlali <abbas.ejlali@iran.ir>
2024-10-06 08:36:48 -0700

    add header to app

--- src/components/template/HomePage.js
+++ src/components/template/HomePage.js
@@ -4,6 +4,9 @@ function HomePage() {
   return (
     <>
       <div className="bg-white rounded-2xl shadow-lg p-8 w-8/12 mx-auto mt-16 border border-neutral-100  border-spacing-2 flex flex-col justify-start items-center  ">
+        <h2 className=" font-bold text-neutral-700 text-5xl mb-10 ">
+          Translation App
+        </h2>
         <div className=" flex flex-row w-full justify-between items-center ">
           <select className="select   select-bordered  w-2/4">
             <option disabled selected>
